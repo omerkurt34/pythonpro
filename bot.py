3e7da11facd66b401e11751861374e9245860ecd
omerkurt34 <144345460+omerkurt34@users.noreply.github.com>
2023-10-14 12:52:34 +0300

    Create bot.py

---
+++ bot.py
@@ -0,0 +1,25 @@
+import discord
+from password import gen_pass
+# ayricaliklar (intents) değişkeni botun ayrıcalıklarını depolayacak
+intents = discord.Intents.default()
+# Mesajları okuma ayrıcalığını etkinleştirelim
+intents.message_content = True
+# client (istemci) değişkeniyle bir bot oluşturalım ve ayrıcalıkları ona aktaralım
+client = discord.Client(intents=intents)
+
+@client.event
+async def on_ready():
+    print(f'We have logged in as {client.user}')
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+    if message.content.startswith('$hello'):
+        await message.channel.send("Hi!")
+    elif message.content.startswith('$ŞİFRE'):
+        await message.channel.send(gen_pass(20))
+    else:
+        await message.channel.send(message.content)
+
+client.run("#tokeniniz burada olacak")
